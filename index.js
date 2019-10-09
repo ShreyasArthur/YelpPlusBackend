1279a93b8d5c392ba5d1d34e40a1678dfc21da17
Dominic <dd429@njit.edu>
2019-10-09 14:48:17 -0400

    Updated port number to allow heroku to assign free ports to the app

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 var express = require('express')
 var mongoose = require('mongoose')
 
-
 // variables
 var app = express()
-var port = 4000
+var port = process.env.PORT || 4000
 
 // DB Connection
 mongoose.connect("mongodb://dom:njit1234@ds331558.mlab.com:31558/heroku_5hf0p9gc", {useNewUrlParser: true}, function(error){
