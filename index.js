d9f1a49f588c0e0b7a423e5318e467375fe8f448
Dominic <dd429@njit.edu>
2019-10-10 21:01:21 -0400

    Seperated user schema into models folder

--- index.js
+++ index.js
@@ -2,60 +2,49 @@
 var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
+var User = require('./models/user')
 
 // variables
 var app = express()
 var port = process.env.PORT || 4000
 
 // DB Connection
-mongoose.connect("mongodb://dom:njit1234@ds331558.mlab.com:31558/heroku_5hf0p9gc", {useNewUrlParser: true}, function(error){
+mongoose.connect("mongodb://dom:njit1234@ds331558.mlab.com:31558/heroku_5hf0p9gc", {useNewUrlParser: true, useUnifiedTopology: true}, function(error){
     if(error) console.log(error)
 })
 
 // middleware
 app.use(bodyParser.urlencoded({extended: true}))
 
-// Schema
-var userScehma = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    email: String,
-    password: String,
-    isOwner: Boolean
-})
-
-// Model
-var User = mongoose.model("User", userScehma)
-    
-// routes
+// Routes
 app.get("/", function(req, res){
     res.send("Welcome to the homepage")
 })
 
+// register route
 app.post("/registerUser", function(req, res){
-    var user = new User({
+    User.create({
         first_name  : req.body.first_name,
         last_name   : req.body.last_name,
         email       : req.body.email,
         password    : req.body.password,
         isOwner     : req.body.isOwner
-    })
-    user.save(function(err){
-        if(err) console.log("Something went wrong")
-        else console.log("We saved user to db")
+    }, function(err, user){
+        if(err) console.log("Something went wrong while registering user")
+        else console.log("User inserted")
     })
     res.send("ok")
 })
 
+// login route
 app.post("/loginUser", function(req, res){
     User.findOne({
         email: req.body.email,
-        password: req.body.password
-    }, function(err, result){
-        if(err) console.log("Error finding user")
-        console.log(result)
+        password: req.body.password 
+    }, function(err, user){
+        if(err) console.log("Something went wrong while logging in")
+        else res.send(user)    
     })
-    res.send("ok")
 })
 
 app.listen(port, function(){
