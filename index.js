9cfeddad0d10d4291650f2748185a0ce6a1a7ebf
Dominic <dd429@njit.edu>
2019-10-29 14:54:09 -0400

    Find business by category had a wrong query fixed that

--- index.js
+++ index.js
@@ -241,7 +241,7 @@ app.get("/business", function(req, res){
 
 // Route: Get business by category
 app.get("/business/:id", function(req, res){
-    Business.findById(req.params.id, function(err, res){
+    Business.find({category: req.params.id}, function(err, business){
         if(err) console.log(err)
         else{
             res.send(business)
